e87908102517fa8f9a82c2a9931f86f09736ae0a
Javier Viola <javier@parity.io>
2023-05-22 09:35:40 -0300

    use flat file tree

--- crates/configuration/src/lib.rs
+++ crates/configuration/src/lib.rs
@@ -2,3 +2,8 @@ pub mod network;
 pub mod relaychain;
 pub mod parachain;
 pub mod shared;
+
+pub use network::NetworkConfig;
+pub use relaychain::RelaychainConfig;
+pub use parachain::ParachainConfig;
+

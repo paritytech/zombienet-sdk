2cf83529b3dd17d42d042e2a0f157c3955464751
wirednkod <wirednkod@gmail.com>
2023-05-25 18:22:58 +0300

    Add more types and native client impl

--- crates/provider/src/native.rs
+++ crates/provider/src/native.rs
@@ -1,11 +1,323 @@
-#[allow(dead_code)]
-pub struct Something {
-    something: String,
+use std::error::Error;
+
+use crate::shared::{
+    client::Client,
+    types::{FileMap, NameSpaceDef, PodDef, RunCommandOptions, RunCommandResponse, Settings},
+};
+
+struct Native {
+    // Namespace of the client
+    namespace:             String,
+    // path where configuration relies
+    config_path:           String,
+    // variable that shows if debug is activated
+    debug:                 bool,
+    // the timeout for the client to exit
+    timeout:               u32,
+    // command sent to client
+    command:               String,
+    // temporary directory
+    tmp_dir:               String,
+    pod_monitor_available: bool,
+    local_magic_file_path: String,
+    // name of the provider
+    provider_name:         String,
 }
 
-impl Default for Something {
+impl Default for Native {
     fn default() -> Self {
-        // [TODO]: define the default value for global settings
+        // [TODO]: define the default value for Native
         todo!()
     }
+}
+
+impl Native {
+    pub fn new(namespace: &str, config_path: &str, tmp_dir: &str) -> Native {
+        Native {
+            namespace:             namespace.to_owned(),
+            config_path:           config_path.to_owned(),
+            debug:                 true,
+            timeout:               60, // seconds
+            tmp_dir:               tmp_dir.to_owned(),
+            command:               "bash".to_owned(),
+            pod_monitor_available: false,
+            local_magic_file_path: format!("{}/finished.txt", tmp_dir),
+            provider_name:         "native".to_owned(),
+        }
+    }
+}
+
+impl Client for Native {
+    fn create_namespace<'async_trait>() -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn static_setup<'async_trait>(
+        settings: Settings,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn destroy_namespace<'async_trait>() -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn get_node_logs<'async_trait>(
+        pod_name: String,
+        since: Option<u32>,
+        with_timestamp: Option<bool>,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<String, Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn dump_logs<'async_trait>(
+        path: String,
+        pod_name: String,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn upsert_cron_job<'async_trait>(
+        minutes: u32,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn start_port_forwarding<'async_trait>(
+        port: u16,
+        identifier: String,
+        namespace: Option<String>,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<u16, Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn run_command<'async_trait>(
+        args: Vec<String>,
+        opts: RunCommandOptions,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<RunCommandResponse, Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn run_script<'async_trait>(
+        identifier: String,
+        script_path: String,
+        args: Vec<String>,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<RunCommandResponse, Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn spawn_from_def<'async_trait>(
+        pod_def: PodDef,
+        files_to_copy: Option<Vec<FileMap>>,
+        keystore: Option<String>,
+        chain_spec_id: Option<String>,
+        db_snapshot: Option<String>,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn copy_file_from_pod<'async_trait>(
+        identifier: String,
+        pod_file_path: String,
+        local_file_path: String,
+        container: Option<String>,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn put_local_magic_file<'async_trait>(
+        name: String,
+        container: Option<String>,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn create_resource<'async_trait>(
+        resourse_def: NameSpaceDef,
+        scoped: bool,
+        wait_ready: bool,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn create_pod_monitor<'async_trait>(
+        file_name: String,
+        chain: String,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn setup_cleaner<'async_trait>() -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn is_pod_monitor_available<'async_trait>() -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), bool>> + core::marker::Send + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn get_pause_args(name: String) -> Vec<String> {
+        todo!()
+    }
+
+    fn get_resume_args(name: String) -> Vec<String> {
+        todo!()
+    }
+
+    fn restart_node<'async_trait>(
+        name: String,
+        timeout: u32,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), bool>> + core::marker::Send + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn get_node_info<'async_trait>(
+        identifier: String,
+        port: Option<u16>,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<Vec<(String, u32)>, Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn get_node_ip<'async_trait>(
+        identifier: String,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<String, Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn spawn_intro_spector<'async_trait>(
+        ws_uri: String,
+    ) -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), Box<dyn Error>>>
+                + core::marker::Send
+                + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn validate_access<'async_trait>() -> core::pin::Pin<
+        Box<
+            dyn core::future::Future<Output = Result<(), bool>> + core::marker::Send + 'async_trait,
+        >,
+    > {
+        todo!()
+    }
+
+    fn get_logs_command(name: String) -> String {
+        todo!()
+    }
+}
+
+fn main() {
+    let mut some: Native = Native::new("namespace", "config_path", "tmp_dir");
 }
--- crates/provider/src/shared/types.rs
+++ crates/provider/src/shared/types.rs
@@ -1,4 +1,30 @@
 #[derive(Debug, Clone, PartialEq)]
+enum ZombieRole {
+    Temp,
+    Node,
+    BootNode,
+    Collator,
+    CumulusCollator,
+    Authority,
+    FullNode,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum PortName {
+    Prometheus,
+    Rpc,
+    RpcWs,
+    P2P,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum ImagePullPolicy {
+    IfNotPresent,
+    Never,
+    Always,
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct FileMap {
     local_file_path:  String,
     remote_file_path: String,
@@ -19,4 +45,99 @@ pub struct RunCommandOptions {
     scoped:       Option<bool>,
     allow_fail:   Option<bool>,
     main_cmd:     String,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct NsLabels {
+    job_id:       String,
+    project_name: String,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct NsMetadata {
+    name:   String,
+    labels: NsLabels,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct NameSpaceDef {
+    api_version: String,
+    kind:        String,
+    metadata:    NsMetadata,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct PodLabels {
+    zombie_role: ZombieRole,
+    app:         String,
+    zombie_ns:   String,
+    name:        String,
+    instance:    String,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct PodMetadata {
+    name:      String,
+    namespace: String,
+    labels:    PodLabels,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct PodSpec {
+    cfg_path:  String,
+    data_path: String,
+    ports:     Vec<Port>,
+    command:   String,
+    env:       ProcessEnvironment,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct PodDef {
+    metadata: PodMetadata,
+    spec:     PodSpec,
+}
+
+// TODO: Fix the ProcessEnvironment nased on the TS below
+// interface ProcessEnvironment {
+//   [key: String]: String;
+// }
+#[derive(Debug, Clone, PartialEq)]
+struct ProcessEnvironment {}
+
+#[derive(Debug, Clone, PartialEq)]
+struct Port {
+    container_port: u16,
+    name:           PortName,
+    flag:           String,
+    host_port:      u16,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct GlobalVolume {
+    name:       String,
+    fs_type:    String,
+    mount_path: String,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Settings {
+    global_volumes:                     Option<Vec<GlobalVolume>>,
+    bootnode:                           Option<bool>,
+    bootnode_domain:                    Option<String>,
+    timeout:                            u16,
+    node_spawn_timeout:                 u16,
+    grafana:                            Option<bool>,
+    telemetry:                          Option<bool>,
+    prometheus:                         Option<bool>,
+    jaeger_agent:                       Option<String>, // agent or collator
+    tracing_collator_url:               Option<String>, // collator query url
+    tracing_collator_service_name:      Option<String>, /* only used by k8s provider and if not set the `url` */
+    tracing_collator_service_namespace: Option<String>, /* only used by k8s provider and if not set the `url` */
+    tracing_collator_service_port:      Option<u16>, /* only used by k8s provider and if not set the `url` */
+    enable_tracing:                     Option<bool>,
+    provider:                           String,
+    polkadot_introspector:              Option<bool>,
+    backchannel:                        Option<bool>, /* only used in k8s at the moment, spawn a backchannel instance */
+    image_pull_policy:                  ImagePullPolicy,
+    local_ip:                           Option<String>, /* ip used for expose local services (rpc/metrics/monitors) */
 }

f2e59213aa6f36e0368d14954f21395614e19dfd
l0r1s <contact@lorismoulin.com>
2023-05-28 01:27:33 +0200

    wip: refacto network

--- crates/configuration/src/network.rs
+++ crates/configuration/src/network.rs
@@ -1,16 +1,26 @@
+use std::marker::PhantomData;
+
 use crate::{
-    shared::types::{Duration, IpAddress, MultiAddress},
+    hrmp_channel::{self, HrmpChannelConfigBuilder},
+    parachain::{self, ParachainConfigBuilder},
+    relaychain::{self, RelaychainConfigBuilder},
+    shared::{
+        macros::states,
+        types::{Duration, IpAddress, MultiAddress},
+    },
     HrmpChannelConfig, ParachainConfig, RelaychainConfig,
 };
 
 /// Global settings applied to an entire network.
+#[derive(Debug, Clone, PartialEq)]
 pub struct GlobalSettings {
     /// Whether we should spawn a dedicated bootnode for each chain.
-    spawn_bootnode: bool,
+    /// TODO: commented now until we decide how we want to use this option
+    // spawn_bootnode: bool,
 
     /// External bootnode address.
-    /// [TODO]: is it a default overriden by node config, maybe an option ?
-    bootnode_address: MultiAddress,
+    /// TODO: is it a default overriden by node config, maybe an option ?
+    bootnodes_addresses: Vec<MultiAddress>,
 
     /// Global spawn timeout in seconds.
     network_spawn_timeout: Duration,
@@ -22,68 +32,108 @@ pub struct GlobalSettings {
     local_ip: Option<IpAddress>,
 }
 
-impl Default for GlobalSettings {
-    fn default() -> Self {
-        // [TODO]: define the default value for global settings
-        todo!()
-    }
-}
-
 /// A network configuration, composed of a relaychain, parachains and HRMP channels.
+#[derive(Debug, Clone, PartialEq)]
 pub struct NetworkConfig {
     /// The global settings applied to the network.
-    global_settings: GlobalSettings,
+    // global_settings: GlobalSettings,
 
-    /// Relaychain configuration.
-    relaychain: RelaychainConfig,
+    // /// Relaychain configuration.
+    // relaychain: RelaychainConfig,
 
     /// Parachains configurations.
     parachains: Vec<ParachainConfig>,
 
     /// HRMP channels configurations.
     hrmp_channels: Vec<HrmpChannelConfig>,
-
-    // [TODO]: what does it represents ?
-    seed: String,
-}
-
-impl Default for NetworkConfig {
-    fn default() -> Self {
-        // [TODO]: define the default value for a network
-        todo!()
-    }
 }
 
 impl NetworkConfig {
-    pub fn new() -> NetworkConfig {
-        Self::default()
+    // pub fn global_settings(&self) -> &GlobalSettings {
+    //     &self.global_settings
+    // }
+
+    // pub fn relaychain(&self) -> &RelaychainConfig {
+    //     &self.relaychain
+    // }
+
+    pub fn parachains(&self) -> Vec<&ParachainConfig> {
+        self.parachains.iter().collect::<Vec<_>>()
     }
 
-    pub fn with_global_settings(self, f: fn(GlobalSettings) -> GlobalSettings) -> Self {
-        Self {
-            global_settings: f(self.global_settings),
-            ..self
+    pub fn hrmp_channels(&self) -> Vec<&HrmpChannelConfig> {
+        self.hrmp_channels.iter().collect::<Vec<_>>()
+    }
+}
+
+states! {
+    Initial,
+    WithRelaychain
+}
+
+#[derive(Debug)]
+pub struct NetworkConfigBuilder<State> {
+    config: NetworkConfig,
+    _state: PhantomData<State>,
+}
+
+impl<A> NetworkConfigBuilder<A> {
+    fn transition<B>(config: NetworkConfig) -> NetworkConfigBuilder<B> {
+        NetworkConfigBuilder {
+            config,
+            _state: PhantomData,
         }
     }
+}
 
-    pub fn with_relaychain(self, f: fn(RelaychainConfig) -> RelaychainConfig) -> Self {
-        Self {
-            relaychain: f(self.relaychain),
-            ..self
+impl NetworkConfigBuilder<Initial> {
+    pub fn new() -> NetworkConfigBuilder<Initial> {
+        NetworkConfigBuilder {
+            config: NetworkConfig {
+                // global_settings: GlobalSettings {
+                //     bootnodes_addresses: vec![],
+                //     network_spawn_timeout: 1000,
+                //     node_spawn_timeout: 300,
+                //     local_ip: None,
+                // },
+                // relaychain: RelaychainConfigBuilder::new()
+                //     .with_chain("")
+                //     .with_node(|node| node.with_name("").with_command(""))
+                //     .build(),
+                parachains: vec![],
+                hrmp_channels: vec![],
+            },
+            _state: PhantomData,
         }
     }
+}
 
-    pub fn with_parachain(self, f: fn(ParachainConfig) -> ParachainConfig) -> Self {
-        Self {
-            parachains: vec![self.parachains, vec![f(ParachainConfig::default())]].concat(),
-            ..self
-        }
+impl NetworkConfigBuilder<WithRelaychain> {
+    pub fn with_parachain(
+        self,
+        f: fn(
+            ParachainConfigBuilder<parachain::Initial>,
+        ) -> ParachainConfigBuilder<parachain::WithAtLeastOneCollator>,
+    ) -> Self {
+        let new_parachain = f(ParachainConfigBuilder::new()).build();
+
+        Self::transition(NetworkConfig {
+            parachains: vec![self.config.parachains, vec![new_parachain]].concat(),
+            ..self.config
+        })
     }
 
-    pub fn with_hrmp_channel(self, f: fn(HrmpChannelConfig) -> HrmpChannelConfig) -> Self {
-        Self {
-            hrmp_channels: vec![self.hrmp_channels, vec![f(HrmpChannelConfig::default())]].concat(),
-            ..self
-        }
+    pub fn with_hrmp_channel(
+        self,
+        f: fn(
+            HrmpChannelConfigBuilder<hrmp_channel::Initial>,
+        ) -> HrmpChannelConfigBuilder<hrmp_channel::WithRecipient>,
+    ) -> Self {
+        let new_hrmp_channel = f(HrmpChannelConfigBuilder::new()).build();
+
+        Self::transition(NetworkConfig {
+            hrmp_channels: vec![self.config.hrmp_channels, vec![new_hrmp_channel]].concat(),
+            ..self.config
+        })
     }
 }

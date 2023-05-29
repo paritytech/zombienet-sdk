be6bc0739c02b3975208f39db9fcf60cb04a21db
l0r1s <contact@lorismoulin.com>
2023-05-29 14:17:11 +0200

    feat: added test for parachain builder

--- crates/configuration/src/parachain.rs
+++ crates/configuration/src/parachain.rs
@@ -222,7 +222,7 @@ impl ParachainConfigBuilder<WithAtLeastOneCollator> {
         })
     }
 
-    pub fn is_cumulus_based(self, choice: bool) -> Self {
+    pub fn cumulus_based(self, choice: bool) -> Self {
         Self::transition(ParachainConfig {
             is_cumulus_based: choice,
             ..self.config
@@ -255,8 +255,84 @@ impl ParachainConfigBuilder<WithAtLeastOneCollator> {
 
 #[cfg(test)]
 mod tests {
-    use super::ParachainConfigBuilder;
+    use crate::shared::types::{AssetLocation, MultiAddress};
+
+    use super::{ParachainConfigBuilder, RegistrationStrategy};
 
     #[test]
-    fn parachain_config_builder_should_build_a_new_parachain_config_correctly() {}
+    fn parachain_config_builder_should_build_a_new_parachain_config_correctly() {
+        let parachain_config = ParachainConfigBuilder::new()
+            .with_id(1000)
+            .with_collator(|collator1| {
+                collator1
+                    .with_name("collator1")
+                    .with_command("command1")
+                    .bootnode(true)
+            })
+            .with_collator(|collator2| {
+                collator2
+                    .with_name("collator2")
+                    .with_command("command2")
+                    .validator(true)
+            })
+            .with_chain("mychainname")
+            .with_registration_strategy(RegistrationStrategy::UsingExtrinsic)
+            .with_initial_balance(100_000_042)
+            .with_genesis_wasm_path(AssetLocation::Url(
+                "https://www.backupsite.com/my/wasm/file.tgz".into(),
+            ))
+            .with_genesis_wasm_generator("my wasm generator command")
+            .with_genesis_state_path(AssetLocation::FilePath("./path/to/genesis/state".into()))
+            .with_genesis_state_generator("my state generator command")
+            .with_chain_spec_path(AssetLocation::FilePath("./path/to/chain/spec.json".into()))
+            .cumulus_based(false)
+            .with_bootnodes_addresses(vec![
+                "/ip4/10.41.122.55/tcp/45421".into(),
+                "/ip4/51.144.222.10/tcp/2333".into(),
+            ])
+            .build();
+
+        assert_eq!(parachain_config.id(), 1000);
+        assert_eq!(parachain_config.collators().len(), 2);
+        let &collator1 = parachain_config.collators().first().unwrap();
+        assert_eq!(collator1.name(), "collator1");
+        assert_eq!(collator1.command().unwrap(), "command1");
+        assert!(collator1.is_bootnode());
+        let &collator2 = parachain_config.collators().last().unwrap();
+        assert_eq!(collator2.name(), "collator2");
+        assert_eq!(collator2.command().unwrap(), "command2");
+        assert!(collator2.is_validator(), "node2");
+        assert_eq!(parachain_config.chain().unwrap(), "mychainname");
+        assert_eq!(
+            parachain_config.registration_strategy().unwrap(),
+            &RegistrationStrategy::UsingExtrinsic
+        );
+        assert_eq!(parachain_config.initial_balance(), 100_000_042);
+        assert!(matches!(
+            parachain_config.genesis_wasm_path().unwrap(),
+            AssetLocation::Url(value) if value == "https://www.backupsite.com/my/wasm/file.tgz"
+        ));
+        assert_eq!(
+            parachain_config.genesis_wasm_generator().unwrap(),
+            "my wasm generator command"
+        );
+        assert!(matches!(
+            parachain_config.genesis_state_path().unwrap(),
+            AssetLocation::FilePath(value) if value == "./path/to/genesis/state"
+        ));
+        assert_eq!(
+            parachain_config.genesis_state_generator().unwrap(),
+            "my state generator command"
+        );
+        assert!(matches!(
+            parachain_config.chain_spec_path().unwrap(),
+            AssetLocation::FilePath(value) if value == "./path/to/chain/spec.json"
+        ));
+        assert!(!parachain_config.is_cumulus_based());
+        let bootnodes_addresses: Vec<MultiAddress> = vec![
+            "/ip4/10.41.122.55/tcp/45421".into(),
+            "/ip4/51.144.222.10/tcp/2333".into(),
+        ];
+        assert_eq!(parachain_config.bootnodes_addresses(), bootnodes_addresses.iter().collect::<Vec<_>>());
+    }
 }

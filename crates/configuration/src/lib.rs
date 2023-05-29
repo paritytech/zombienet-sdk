c5e4707b8b2f45816da68038d6704901b6708a3d
l0r1s <contact@lorismoulin.com>
2023-05-29 19:10:32 +0200

    feat: refactored exports from lib

--- crates/configuration/src/lib.rs
+++ crates/configuration/src/lib.rs
@@ -1,10 +1,12 @@
+mod global_settings;
 mod hrmp_channel;
 mod network;
 mod parachain;
 mod relaychain;
 mod shared;
 
-pub use network::NetworkConfig;
-pub use parachain::ParachainConfig;
-pub use relaychain::RelaychainConfig;
-pub use hrmp_channel::HrmpChannelConfig;
+pub use global_settings::{GlobalSettings, GlobalSettingsBuilder};
+pub use hrmp_channel::{HrmpChannelConfig, HrmpChannelConfigBuilder};
+pub use network::{NetworkConfig, NetworkConfigBuilder};
+pub use parachain::{ParachainConfig, ParachainConfigBuilder};
+pub use relaychain::{RelaychainConfig, RelaychainConfigBuilder};

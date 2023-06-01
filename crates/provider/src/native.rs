12d50199257e4bfe03da0f9b5602071fc90a2e62
wirednkod <wirednkod@gmail.com>
2023-06-01 21:44:40 +0300

    address more comments

--- crates/provider/src/native.rs
+++ crates/provider/src/native.rs
@@ -31,23 +31,23 @@ struct FilesystemInMemory {}
 
 #[derive(Debug, Serialize, Clone, PartialEq)]
 struct NativeProvider<T: FileSystem + Debug> {
-    /// Namespace of the client
-    namespace:             String,
-    /// path where configuration relies
-    config_path:           String,
-    // variable that shows if debug is activated
-    debug:                 bool,
-    /// the timeout for the client to exit
-    timeout:               u32,
-    /// command sent to client
-    command:               String,
-    /// temporary directory
-    tmp_dir:               String,
-    pod_monitor_available: bool,
-    local_magic_file_path: String,
-    remote_dir:            String,
-    data_dir:              String,
-    filesystem:            T,
+    // Namespace of the client
+    namespace:                String,
+    // Path where configuration relies
+    config_path:              String,
+    // Variable that shows if debug is activated
+    is_debug:                 bool,
+    // The timeout for the client to exit
+    timeout:                  u32,
+    // Command sent to client
+    command:                  String,
+    // Temporary directory
+    tmp_dir:                  String,
+    is_pod_monitor_available: bool,
+    local_magic_file_path:    String,
+    remote_dir:               String,
+    data_dir:                 String,
+    filesystem:               T,
 }
 
 impl<T: FileSystem + Debug> NativeProvider<T> {
@@ -57,19 +57,19 @@ impl<T: FileSystem + Debug> NativeProvider<T> {
         tmp_dir: impl Into<String>,
         filesystem: T,
     ) -> Self {
-        let tmp_dir = tmp_dir.into();
+        let tmp_dir: String = tmp_dir.into();
 
         Self {
             namespace: namespace.into(),
             config_path: config_path.into(),
-            debug: true,
+            is_debug: true,
             timeout: 60, // seconds
             local_magic_file_path: format!("{}/finished.txt", &tmp_dir),
             remote_dir: format!("{}{}", &tmp_dir, DEFAULT_REMOTE_DIR),
             data_dir: format!("{}{}", &tmp_dir, DEFAULT_DATA_DIR),
             command: "bash".into(),
             tmp_dir,
-            pod_monitor_available: false,
+            is_pod_monitor_available: false,
             filesystem,
         }
     }
@@ -78,7 +78,7 @@ impl<T: FileSystem + Debug> NativeProvider<T> {
 #[async_trait]
 impl<T: FileSystem + Debug> Provider for NativeProvider<T> {
     fn create_namespace(&mut self) -> Result<(), Box<dyn Error>> {
-        let name_space_def: NamespaceDef = NamespaceDef {
+        let namespace_def: NamespaceDef = NamespaceDef {
             api_version: "v1".into(),
             kind:        "Namespace".into(),
             metadata:    NamespaceMetadata {
@@ -87,8 +87,8 @@ impl<T: FileSystem + Debug> Provider for NativeProvider<T> {
             },
         };
 
-        let file_path: String = format!("{}/{}", &self.tmp_dir, "namespace");
-        let content: String = serde_json::to_string(&name_space_def)?;
+        let file_path: String = format!("{}/namespace", &self.tmp_dir);
+        let content: String = serde_json::to_string(&namespace_def)?;
 
         self.filesystem.write(file_path, content)?;
         self.filesystem.create_dir(&self.remote_dir)?;
@@ -236,11 +236,11 @@ mod tests {
 
         assert_eq!(native_provider.namespace, "something");
         assert_eq!(native_provider.config_path, "./");
-        assert!(native_provider.debug);
+        assert!(native_provider.is_debug);
         assert_eq!(native_provider.timeout, 60);
         assert_eq!(native_provider.tmp_dir, "./tmp");
         assert_eq!(native_provider.command, "bash");
-        assert!(!native_provider.pod_monitor_available);
+        assert!(!native_provider.is_pod_monitor_available);
         assert_eq!(native_provider.local_magic_file_path, "./tmp/finished.txt");
         assert_eq!(native_provider.remote_dir, "./tmp/cfg");
         assert_eq!(native_provider.data_dir, "./tmp/data");

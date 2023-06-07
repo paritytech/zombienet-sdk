7caacaab7dc0087930093e79d0a47dacdfc4e998
l0r1s <contact@lorismoulin.com>
2023-06-08 00:44:42 +0200

    feat: replaced MultiAddress & IpAddress  with stdlib/crate types, used Url/PathBuf for AssetLocation, fixed possible conversion

--- crates/configuration/src/shared/types.rs
+++ crates/configuration/src/shared/types.rs
@@ -1,17 +1,6 @@
-#[derive(Debug, Clone, PartialEq)]
-pub struct MultiAddress(String);
+use std::{path::PathBuf, str::FromStr};
 
-impl MultiAddress {
-    pub fn value(&self) -> &str {
-        &self.0
-    }
-}
-
-impl From<&str> for MultiAddress {
-    fn from(value: &str) -> Self {
-        Self(value.to_owned())
-    }
-}
+use url::Url;
 
 pub type Duration = u32;
 
@@ -21,15 +10,52 @@ pub type ParaId = u32;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum AssetLocation {
-    Url(String),
-    FilePath(String),
+    Url(Url),
+    FilePath(PathBuf),
+}
+
+impl From<Url> for AssetLocation {
+    fn from(value: Url) -> Self {
+        Self::Url(value)
+    }
+}
+
+impl From<PathBuf> for AssetLocation {
+    fn from(value: PathBuf) -> Self {
+        Self::FilePath(value)
+    }
+}
+
+impl TryFrom<&str> for AssetLocation {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if let Ok(parsed_url) = Url::parse(value) {
+            return Ok(Self::Url(parsed_url));
+        }
+
+        if let Ok(parsed_path) = PathBuf::from_str(value) {
+            return Ok(Self::FilePath(parsed_path));
+        }
+
+        Err("unable to convert into url::Url or path::PathBuf".to_string())
+    }
 }
 
 impl AssetLocation {
-    pub fn value(&self) -> &str {
-        match self {
-            AssetLocation::Url(value) => value,
-            AssetLocation::FilePath(value) => value,
+    pub fn as_url(&self) -> Option<&Url> {
+        if let Self::Url(url) = self {
+            Some(url)
+        } else {
+            None
+        }
+    }
+
+    pub fn as_path_buf(&self) -> Option<&PathBuf> {
+        if let Self::FilePath(path) = self {
+            Some(path)
+        } else {
+            None
         }
     }
 }

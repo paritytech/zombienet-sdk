65360a9b925412939e2d490f3cf3f4896a3e2106
l0r1s <contact@lorismoulin.com>
2023-06-14 18:38:43 +0100

    feat: added helpers to merge errors into vec and vec of errors

--- crates/configuration/src/shared/helpers.rs
+++ crates/configuration/src/shared/helpers.rs
@@ -1,3 +1,21 @@
-pub fn merge_errors(errors: Vec<String>, new_error: String) -> Vec<String> {
-    vec![errors, vec![new_error]].concat()
+use std::error::Error;
+
+pub fn merge_errors(errors: Vec<Box<dyn Error>>, new_error: Box<dyn Error>) -> Vec<Box<dyn Error>> {
+    let mut errors = errors;
+    errors.push(new_error);
+
+    errors
+}
+
+pub fn merge_errors_vecs(
+    errors: Vec<Box<dyn Error>>,
+    new_errors: Vec<Box<dyn Error>>,
+) -> Vec<Box<dyn Error>> {
+    let mut errors = errors;
+
+    for new_error in new_errors.into_iter() {
+        errors.push(new_error);
+    }
+
+    errors
 }

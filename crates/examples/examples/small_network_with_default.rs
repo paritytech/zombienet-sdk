f622db9c2fc410fcb1d375a2448147a79e1f0ced
Javier Viola <javier@parity.io>
2023-08-08 08:10:24 -0300

    add examples

---
+++ crates/examples/examples/small_network_with_default.rs
@@ -0,0 +1,14 @@
+use configuration::NetworkConfigBuilder;
+
+fn main() {
+    let config = NetworkConfigBuilder::new()
+        .with_relaychain(|r| {
+            r.with_chain("rococo-local")
+                .with_default_command("polkadot")
+                .with_node(|node| node.with_name("alice"))
+                .with_node(|node| node.with_name("name"))
+        })
+        .build();
+
+    println!("{:?}", config.unwrap());
+}

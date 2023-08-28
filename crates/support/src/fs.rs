b20050ebd98b9dc8633b9e39e3d57e2a3db595ab
l0r1s <contact@lorismoulin.com>
2023-08-28 15:43:55 +0300

    feat: refactored FileSystem trait and errors

--- crates/support/src/fs.rs
+++ crates/support/src/fs.rs
@@ -2,22 +2,25 @@ use std::{ffi::OsString, path::Path};
 
 use async_trait::async_trait;
 
-mod local_file;
 #[cfg(test)]
-pub mod mock;
+pub mod in_memory;
 
 #[derive(Debug, thiserror::Error)]
 pub enum FileSystemError {
-    #[error("File path '{0:?}' doesn't contains UTF-8")]
-    InvalidUtf8Path(OsString),
-    #[error("File '{0}' doesn't contains UTF-8")]
-    InvalidUtf8File(String),
-    #[error("File '{0}' already exists")]
-    FileAlreadyExists(String),
-    #[error("File '{0}' not found")]
-    FileNotFound(String),
-    #[error("File '{0}' is a directory")]
-    FileIsDirectory(String),
+    #[error("File {0:?} already exists")]
+    FileAlreadyExists(OsString),
+    #[error("Directory {0:?} already exists")]
+    DirectoryAlreadyExists(OsString),
+    #[error("Ancestor {0:?} doesn't exists")]
+    AncestorDoesntExists(OsString),
+    #[error("Ancestor {0:?} is not a directory")]
+    AncestorNotDirectory(OsString),
+    #[error("File {0:?} not found")]
+    FileNotFound(OsString),
+    #[error("File {0:?} is a directory")]
+    FileIsDirectory(OsString),
+    #[error("Invalid UTF-8 encoding for file {0:?}")]
+    InvalidUtf8FileEncoding(OsString),
 }
 
 pub type FileSystemResult<T> = Result<T, FileSystemError>;
@@ -34,7 +37,7 @@ pub trait FileSystem {
 
     async fn create_dir_all(&self, path: impl AsRef<Path> + Send) -> FileSystemResult<()>;
 
-    async fn read(&self, path: impl AsRef<Path> + Send) -> FileSystemResult<Option<Vec<u8>>>;
+    async fn read(&self, path: impl AsRef<Path> + Send) -> FileSystemResult<Vec<u8>>;
 
     async fn read_to_string(&self, path: impl AsRef<Path> + Send) -> FileSystemResult<String>;
 

b4309828d46a938a4642dd15142fe9c847e45034
Javier Viola <javier@parity.io>
2023-09-19 19:48:17 -0300

    wip, example to drive exec

--- crates/examples/examples/small_network_with_default.rs
+++ crates/examples/examples/small_network_with_default.rs
@@ -1,14 +1,28 @@
 use configuration::NetworkConfigBuilder;
+use orchestrator::Orchestrator;
+use provider::NativeProvider;
+use support::fs::local::LocalFileSystem;
 
-fn main() {
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
     let config = NetworkConfigBuilder::new()
         .with_relaychain(|r| {
             r.with_chain("rococo-local")
                 .with_default_command("polkadot")
                 .with_node(|node| node.with_name("alice"))
-                .with_node(|node| node.with_name("name"))
+                .with_node(|node| node.with_name("bob"))
         })
-        .build();
+        .build().unwrap();
 
-    println!("{:?}", config.unwrap());
+    println!("{:?}", &config);
+
+    let fs = LocalFileSystem;
+    let provider = NativeProvider::new(fs.clone());
+    let orchestrator = Orchestrator::new(fs, provider);
+    orchestrator.spawn(config).await?;
+    while true {
+
+    }
+    Ok(())
 }

1fbe53f47060d8b684f425a75e43843b9c66b5db
Javier Viola <javier@parity.io>
2023-11-28 15:56:50 -0300

    add example for adding a para to a running network

---
+++ crates/examples/examples/add_para.rs
@@ -0,0 +1,59 @@
+use futures::stream::StreamExt;
+use zombienet_sdk::{NetworkConfigBuilder, NetworkConfigExt};
+use anyhow::anyhow;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    tracing_subscriber::fmt::init();
+    let mut network = NetworkConfigBuilder::new()
+        .with_relaychain(|r| {
+            r.with_chain("rococo-local")
+                .with_default_command("polkadot")
+                .with_node(|node| node.with_name("alice"))
+                .with_node(|node| node.with_name("bob"))
+        })
+        // .with_parachain(|p| {
+        //     p.with_id(100)
+        //         .cumulus_based(true)
+        //         .with_collator(|n| n.with_name("collator").with_command("polkadot-parachain"))
+        // })
+        .build()
+        .unwrap()
+        .spawn_native()
+        .await?;
+
+    println!("🚀🚀🚀🚀 network deployed");
+
+    let alice = network.get_node("alice")?;
+    let client = alice.client::<subxt::PolkadotConfig>().await?;
+
+    // wait 3 blocks
+    let mut blocks = client.blocks().subscribe_finalized().await?.take(3);
+
+    while let Some(block) = blocks.next().await {
+        println!("Block #{}", block?.header().number);
+    }
+
+    println!("⚙️  adding parachain to the running network");
+
+    let para_config = network.para_config_builder()
+        .with_id(100)
+        .with_default_command("polkadot-parachain")
+        .with_collator(|c| {
+            c.with_name("col-100-1")
+        })
+        .build()
+        .map_err(|_e| {
+            anyhow!("Building config")
+        })?;
+
+    network.add_parachain(&para_config, None).await?;
+
+    // For now let just loop....
+    #[allow(clippy::empty_loop)]
+    loop {}
+
+    #[allow(clippy::unreachable)]
+    #[allow(unreachable_code)]
+    Ok(())
+}

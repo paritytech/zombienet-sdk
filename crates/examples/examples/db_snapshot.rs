1c198a27f08e369b0178e4d99632a9cadeb1aa0d
Javier Viola <javier@parity.io>
2024-03-03 12:41:58 -0300

    working example

--- crates/examples/examples/db_snapshot.rs
+++ crates/examples/examples/db_snapshot.rs
@@ -6,7 +6,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut _network = NetworkConfigBuilder::new()
         .with_relaychain(|r| {
             r.with_chain("rococo-local")
-                .with_default_command("substrate")
+                .with_default_command("substrate-node")
                 .with_default_image("docker.io/paritypr/substrate:3428-e5be9c93")
                 .with_default_db_snapshot("https://storage.googleapis.com/zombienet-db-snaps/substrate/0001-basic-warp-sync/chains-9677807d738b951e9f6c82e5fd15518eb0ae0419.tgz")
                 .with_chain_spec_path("/Users/pepo/parity/polkadot-sdk/substrate/zombienet/0001-basic-warp-sync/chain-spec.json")
@@ -25,5 +25,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // For now let just loop....
     #[allow(clippy::empty_loop)]
     loop {}
-
 }

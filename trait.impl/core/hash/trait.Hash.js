c96c5f92a0d4abb9d4e20e2445f342e16de9d84b
GitHub Action <github-action@users.noreply.github.com>
2025-02-13 11:35:05 +0000

    Documentation

---
+++ trait.impl/core/hash/trait.Hash.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["zombienet_prom_metrics_parser",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"zombienet_prom_metrics_parser/enum.Rule.html\" title=\"enum zombienet_prom_metrics_parser::Rule\">Rule</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[309]}
---
+++ trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["zombienet_prom_metrics_parser",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"zombienet_prom_metrics_parser/enum.Rule.html\" title=\"enum zombienet_prom_metrics_parser::Rule\">Rule</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[313]}

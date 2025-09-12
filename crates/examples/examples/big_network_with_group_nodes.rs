35956c6e7dcf3a0547ec473daa7d288e076c8a72
Marios Christou <marios@parity.io>
2025-09-12 16:00:18 +0300

    Add example - network with group nodes generated in code

---
+++ crates/examples/examples/big_network_with_group_nodes.rs
@@ -0,0 +1,48 @@
+use zombienet_sdk::{NetworkConfigBuilder, NetworkConfigExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+    let network = NetworkConfigBuilder::new()
+        .with_relaychain(|r| {
+            r.with_chain("rococo-local")
+                .with_default_command("polkadot")
+                .with_group(|g| {
+                    g.with_count(3)
+                        .with_base_node(|b| b.with_name("relay_group"))
+                })
+        })
+        .with_parachain(|p| {
+            p.with_id(100)
+                .cumulus_based(true)
+                .with_default_command("polkadot-parachain")
+                .with_collator_group(|g| {
+                    g.with_count(3)
+                        .with_base_node(|b| b.with_name("para_group"))
+                })
+                .with_collator_group(|f| {
+                    f.with_count(2)
+                        .with_base_node(|b| b.with_name("para_group-2"))
+                })
+        })
+        .build()
+        .unwrap()
+        .spawn_native()
+        .await?;
+
+    println!("🚀🚀🚀🚀 network deployed");
+
+    let nodes = network.relaychain().nodes();
+    nodes.iter().for_each(|node| {
+        println!("Relay node: {}", node.name());
+    });
+
+    let collators = network.parachains()[0].collators();
+    collators.iter().for_each(|collator| {
+        println!("Collator: {}", collator.name());
+    });
+
+    #[allow(clippy::empty_loop)]
+    loop {}
+
+}

cd35a7229eda910a845b5e43068c0807f22d858f
RafalMirowski1 <rafal@parity.io>
2026-01-09 17:43:21 +0100

    add mixed test

--- crates/sdk/tests/client-runtime.rs
+++ crates/sdk/tests/client-runtime.rs
@@ -6,6 +6,9 @@ use zombienet_sdk::NetworkConfigExt;
 
 fn small_network() -> NetworkConfig {
     let relay_runtime_path = PathBuf::from(env::var("RELAY_RUNTIME_PATH").unwrap());
+    let polkadot_bin_latest = env::var("POLKADOT_BIN_LATEST").unwrap_or("polkadot".into());
+    let polkadot_bin_latest_1 = env::var("POLKADOT_BIN_LATEST-1").unwrap_or("polkadot".into());
+    let polkadot_bin_latest_2 = env::var("POLKADOT_BIN_LATEST-2").unwrap_or("polkadot".into());
 
     NetworkConfigBuilder::new()
         .with_relaychain(|r| {
@@ -13,8 +16,18 @@ fn small_network() -> NetworkConfig {
                 .with_default_command("polkadot")
                 .with_default_args(vec!["-lparachain=debug,runtime=debug".into()])
                 .with_chain_spec_runtime(relay_runtime_path, None)
-                .with_validator(|node| node.with_name("alice"))
-                .with_validator(|node| node.with_name("bob"))
+                .with_validator(|node| {
+                    node.with_name("alice")
+                        .with_command(polkadot_bin_latest.as_ref())
+                })
+                .with_validator(|node| {
+                    node.with_name("bob")
+                        .with_command(polkadot_bin_latest_1.as_ref())
+                })
+                .with_validator(|node| {
+                    node.with_name("charlie")
+                        .with_command(polkadot_bin_latest_2.as_ref())
+                })
         })
         .build()
         .unwrap()
@@ -29,14 +42,21 @@ async fn ci_native_smoke_should_works() {
     let alice = network.get_node("alice").unwrap();
     let alice_client: OnlineClient<PolkadotConfig> = alice.wait_client().await.unwrap();
 
-    let mut blocks = alice_client
-        .blocks()
-        .subscribe_finalized()
-        .await
-        .unwrap()
-        .take(10);
+    let bob = network.get_node("bob").unwrap();
+    let bob_client: OnlineClient<PolkadotConfig> = bob.wait_client().await.unwrap();
+
+    let charlie = network.get_node("charlie").unwrap();
+    let charlie_client: OnlineClient<PolkadotConfig> = charlie.wait_client().await.unwrap();
+
+    wait_n_blocks(&alice_client, 5, "alice").await;
+    wait_n_blocks(&bob_client, 5, "bob").await;
+    wait_n_blocks(&charlie_client, 5, "charlie").await;
+}
+
+async fn wait_n_blocks(client: &OnlineClient<PolkadotConfig>, n: usize, name: &str) {
+    let mut blocks = client.blocks().subscribe_finalized().await.unwrap().take(n);
 
     while let Some(block) = blocks.next().await {
-        println!("Block #{}", block.unwrap().header().number);
+        println!("{name} Block #{}", block.unwrap().header().number);
     }
 }

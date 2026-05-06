78e1e0a25d55854cc9c2243d6a547e83f0540893
GitHub Action <github-action@users.noreply.github.com>
2026-05-06 10:36:52 +0000

    Documentation

---
+++ api/trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["zombienet_orchestrator",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"zombienet_orchestrator/generators/core_assignment/struct.ParaId.html\" title=\"struct zombienet_orchestrator::generators::core_assignment::ParaId\">ParaId</a>",0]]],["zombienet_prom_metrics_parser",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"zombienet_prom_metrics_parser/enum.Rule.html\" title=\"enum zombienet_prom_metrics_parser::Rule\">Rule</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[374,328]}
